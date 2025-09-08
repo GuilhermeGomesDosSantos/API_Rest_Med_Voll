c3609f5573a6f27d66cf6b1a608d64b06fd54ba2
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-09-07 22:37:16 -0300

    Migrations com Flyway

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -10,14 +10,24 @@ import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class Endereco {
     private String logradouro;
-    private String bairo;
+    private String bairro;
     private String cep;
     private String cidade;
     private String uf;
     private String complemento;
     private String numero;
+
+    public Endereco() {}
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
 }
