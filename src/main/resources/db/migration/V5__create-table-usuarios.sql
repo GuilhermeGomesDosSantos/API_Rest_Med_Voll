61061cd772874e93addef0991fcda09f919bc960
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-09-19 23:12:50 -0300

    Controller de Autenticação

--- src/main/resources/db/migration/V5__create-table-usuarios.sql
+++ src/main/resources/db/migration/V5__create-table-usuarios.sql
@@ -1,6 +1,6 @@
 create table usuarios(
     id bigint NOT NULL auto_increment,
-    usuario VARCHAR(100) NOT NULL,
+    login VARCHAR(100) NOT NULL,
     senha VARCHAR(255) NOT NULL,
 
     primary key(id)
