7c9cda6e8a9ad3a4e1afe2f7929fac0f511e5739
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-09-20 19:37:25 -0300

    Validando o token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,10 +12,15 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT= recuperarToken(request);
-        System.out.println(tokenJWT);
+
+        var subject = tokenService.getSubject(tokenJWT);
+        System.out.println(subject);
         filterChain.doFilter(request, response);
 
     }
@@ -25,6 +31,6 @@ public class SecurityFilter extends OncePerRequestFilter {
         if (autorizationHeader == null){
             throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
         }
-        return autorizationHeader.replace("Bearer","");
+        return autorizationHeader.replace("Bearer","").trim();
     }
 }
